935f9384131b557a7c15a093ba43452b657ca27c
Piter <tor10050@mail.ru>
2022-01-17 17:17:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// Package modules
 import React from 'react';
 
 import './styles/global.scss';
 
+// Local modules
 import Container from "./components/Container/Container";
 import Welcome from "./components/Welcome/Welcome";
-import About from "./components/About/About";
+import { About } from "./components/About/About";
 
 const App = () => {
     return (
